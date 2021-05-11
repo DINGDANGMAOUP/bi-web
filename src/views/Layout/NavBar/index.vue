8f2eea74b8f4a2252a9f9b845b866d16609dfa8a
kuroneko <964621334@qq.com>
2021-05-11 14:45:58 +0800

    顶部返回组件

--- src/views/Layout/NavBar/index.vue
+++ src/views/Layout/NavBar/index.vue
@@ -1,62 +1,101 @@
 <template>
   <v-main class="grey lighten-2" style="padding-top: 0px">
     <v-container>
-      <v-row>
+      <v-row style="position: relative" >
         <v-col>
-          <v-card class="mx-auto"
-                  max-width="500"
+          <v-card class="pink lighten-2 mx-auto"
                   outlined
-                  v-for="(first,fIndex) in list" :key="fIndex" v-if="fIndex%3===0">
+                  v-for="(first,fIndex) in list"
+                  :key="fIndex"
+                  v-if="fIndex%3===0"
+          >
             <v-card-title>{{ first.rname }}</v-card-title>
             <v-list
-                    nav
-                    subheader
-                    dense
-                    v-for="(second,sIndex) in first.children"
-                    :key="sIndex">
-               <v-list-item align="left"><v-list-item-title v-text="second.rname"></v-list-item-title></v-list-item>
-              <v-list-item  v-for="(third,tIndex) in second.children" :key="tIndex" @click="Jump(third.rurl)">
-                <v-list-item-subtitle align="left" v-text="third.rname"></v-list-item-subtitle>
-              </v-list-item>
+              class="pink lighten-5 "
+              nav
+              subheader
+              v-for="(second,sIndex) in first.children"
+              :key="sIndex">
+              <!--               <v-list-item  align="left">-->
+              <!--                 <v-card-text align="left" v-text="second.rname"></v-card-text>-->
+              <!--              <v-list-item-title v-text="second.rname"></v-list-item-title>-->
+              <!--               </v-list-item>-->
+              <v-list-group
+                :value="true"
+              >
+                <template
+                  v-slot:activator
+                >
+                  <v-list-item align="left">
+                    <v-list-item-title v-text="second.rname"></v-list-item-title>
+                  </v-list-item>
+                </template>
+                <v-list-item class="pa-0" v-for="(third,tIndex) in second.children" :key="tIndex"
+                             @click="Jump(third.rurl)">
+                  <v-icon>mdi-file-table-outline</v-icon>
+                  <v-list-item-subtitle class="text-sm-left" align="left" v-text="third.rname"></v-list-item-subtitle>
+                </v-list-item>
+              </v-list-group>
+
             </v-list>
           </v-card>
         </v-col>
         <v-col>
           <v-card
-            class="mx-auto"
-            max-width="500"
+            class="mx-auto indigo lighten-2"
             outlined
             v-for="(first,fIndex) in list" :key="fIndex" v-if="fIndex%3===1">
             <v-card-title>{{ first.rname }}</v-card-title>
-            <v-list v-for="(second,sIndex) in first.children" :key="sIndex">
-              <v-list-item-title>{{ second.rname }}</v-list-item-title>
-              <v-chip
-                label
-                outlined
-                v-for="(third,tIndex) in second.children" :key="tIndex" @click="Jump(third.rurl)"
+            <v-list
+              class="indigo lighten-5"
+              nav
+              subheader
+              v-for="(second,sIndex) in first.children"
+              :key="sIndex">
+              <v-list-group
+                :value="true"
               >
-                {{ third.rname }}
-              </v-chip>
+                <template
+                  v-slot:activator
+                >
+                  <v-list-item align="left">
+                    <v-list-item-title v-text="second.rname"></v-list-item-title>
+                  </v-list-item>
+                </template>
+                <v-list-item v-for="(third,tIndex) in second.children" :key="tIndex" @click="Jump(third.rurl)">
+                  <v-icon>mdi-file-table-outline</v-icon>
+                  <v-list-item-subtitle align="left" v-text="third.rname"></v-list-item-subtitle>
+                </v-list-item>
+              </v-list-group>
             </v-list>
           </v-card>
         </v-col>
         <v-col>
           <v-card
-            class="mx-auto"
-            max-width="500"
+            class="mx-auto light-green lighten-2"
             outlined
             v-for="(first,fIndex) in list" :key="fIndex" v-if="fIndex%3===2">
             <v-card-title>{{ first.rname }}</v-card-title>
-            <v-list  v-for="(second,sIndex) in first.children" :key="sIndex">
-              <v-list-item-title>{{ second.rname }}</v-list-item-title>
-              <v-list-item v-for="(third,tIndex) in second.children" :key="tIndex" @click="Jump(third.rurl)">
-                <v-chip
-                  label
-                  outlined
-                >
-                  {{ third.rname }}
-                </v-chip>
-              </v-list-item>
+            <v-list
+              class="light-green lighten-5"
+              nav
+              subheader
+
+              v-for="(second,sIndex) in first.children"
+              :key="sIndex">
+              <v-list-group
+                :value="true"
+              >
+                <template v-slot:activator>
+                  <v-list-item align="left">
+                    <v-list-item-title v-text="second.rname"></v-list-item-title>
+                  </v-list-item>
+                </template>
+                <v-list-item v-for="(third,tIndex) in second.children" :key="tIndex" @click="Jump(third.rurl)">
+                  <v-icon>mdi-file-table-outline</v-icon>
+                  <v-list-item-subtitle align="left" v-text="third.rname"></v-list-item-subtitle>
+                </v-list-item>
+              </v-list-group>
             </v-list>
           </v-card>
         </v-col>
@@ -100,6 +139,24 @@
       <!--          </v-expansion-panels>-->
       <!--        </v-col>-->
       <!--      </v-row>-->
+      <v-card-text style="position: fixed;z-index: 600;right: 0px; bottom: 40px;">
+      <v-fab-transition>
+        <v-btn
+          v-if="btnFlag"
+          small
+          color="green"
+          fab
+          dark
+          absolute
+          bottom
+          right
+          class="my-2"
+          @click="backTop"
+        >
+          <v-icon>mdi-chevron-up</v-icon>
+        </v-btn>
+      </v-fab-transition>
+      </v-card-text>
     </v-container>
   </v-main>
 
@@ -114,7 +171,10 @@ export default {
     return {
       sticky: false,
       panel: [],
-      list: []
+      list: [],
+      btnFlag: false,
+      // 记录屏幕滑动的值
+      scrollTop: 0
     }
   },
   methods: {
@@ -144,13 +204,42 @@ export default {
       // this.$router.push('/iframe')
       // window.location.href(url)
       window.open(url)
+    },
+
+    // 点击图片回到顶部方法，加计时器是为了过渡顺滑
+    backTop () {
+      const that = this
+      let timer = setInterval(() => {
+        let ispeed = Math.floor(-that.scrollTop / 5)
+        document.documentElement.scrollTop = document.body.scrollTop = that.scrollTop + ispeed
+        if (that.scrollTop === 0) {
+          clearInterval(timer)
+        }
+      }, 16)
+    },
+
+    // 为了计算距离顶部的高度，当高度大于60显示回顶部图标，小于60则隐藏
+    scrollToTop () {
+      const that = this
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      that.scrollTop = scrollTop
+      if (that.scrollTop > 60) {
+        that.btnFlag = true
+      } else {
+        that.btnFlag = false
+      }
     }
+
   },
   created () {
     this.getTree()
   },
   mounted () {
     this.getTree()
+    window.addEventListener('scroll', this.scrollToTop)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.scrollToTop)
   }
 }
 </script>
